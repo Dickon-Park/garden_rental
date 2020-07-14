617a1efd71c104ab9085da11135c65044b101718
Dickon Park <dickon.j.park@gmail.com>
2020-07-14 21:54:28 +0200

    reviews

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,20 @@
+class ReviewsController < ApplicationController
+  def show
+    @review = Review.find(params[:id])
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    if @review.save
+      redirect_to review_path(@review)
+    else
+      render "new"
+    end
+  end
+
+  
+end
