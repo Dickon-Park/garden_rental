10c373afc6d31dc10d77680e1ada411f817c011f
Arnaud Verheyen <arnaud_verheyen@outlook.com>
2020-07-16 21:02:03 +0200

    index new update

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'index/show'
+  get 'index/new'
+  get 'index/edit'
+  get 'index/delete'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
