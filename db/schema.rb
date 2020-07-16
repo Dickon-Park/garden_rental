8b59355c8d89f94318bf995140135d26335a3c9d
raissa borba <raissavborba@gmail.com>
2020-07-16 19:45:35 +0200

    New db and bookings model

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_14_182826) do
+ActiveRecord::Schema.define(version: 2020_07_16_173824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bookings", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
+  end
+
   create_table "gardens", force: :cascade do |t|
-    t.string "index"
-    t.string "new"
-    t.string "create"
+    t.string "name"
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
