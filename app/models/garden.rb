0c808185826d2fa3e05aeda073bcf81863853a1d
raissa borba <raissavborba@gmail.com>
2020-07-16 20:00:11 +0200

    booking route

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,2 +1,4 @@
 class Garden < ApplicationRecord
+    has_many :reviews, dependent: :destroy
+    has_many :bookings, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :gardens
+  resources :gardens do 
+    resources :booking, only: [ :show, :new, :create, :edit, :update, :destroy ]
+  end
   devise_for :users
   root to: 'pages#home'
   resources :reviews
