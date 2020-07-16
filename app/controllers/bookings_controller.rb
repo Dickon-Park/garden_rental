c4a1aab478224e30d9cf50bb31c99ad6e3219621
raissa borba <raissavborba@gmail.com>
2020-07-16 20:35:06 +0200

    booking controller added create

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,7 @@
 class BookingsController < ApplicationController
+    def new
+      # we need @restaurant in our `simple_form_for`
+      @garden = garden.find(params[:garden_id])
+      @booking = Booking.new
+    end
 end
