e42f6ffbb794fa006e99ad59d0794a2b73f56cff
raissa borba <raissavborba@gmail.com>
2020-07-21 19:38:44 +0200

    added an upload image button, the cloudinary config and price capacity to garden table

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,7 +1,8 @@
 class Garden < ApplicationRecord
+    has_one_attached :photo
     has_many :bookings, dependent: :destroy
     has_many :reviews, through: :bookings, dependent: :destroy
     belongs_to :user
 
-    validates :name, :description, presence: true
+    validates :name, :description, :price, :capacity, presence: true
 end
