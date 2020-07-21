078164b943584b7953baa8236f895e5420e3b98a
Dickon Park <dickon.j.park@gmail.com>
2020-07-21 21:03:35 +0200

    geocode

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,7 +1,10 @@
 class Garden < ApplicationRecord
+    
     has_many :bookings, dependent: :destroy
     has_many :reviews, through: :bookings, dependent: :destroy
     belongs_to :user
-
     validates :name, :description, presence: true
+    geocoded_by :address
+    after_validation :geocode, if: :will_save_change_to_address?
+   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_18_151045) do
+ActiveRecord::Schema.define(version: 2020_07_21_102414) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,9 @@ ActiveRecord::Schema.define(version: 2020_07_18_151045) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id"
+    t.float "latitude"
+    t.float "longitude"
+    t.string "address"
     t.index ["user_id"], name: "index_gardens_on_user_id"
   end
 
