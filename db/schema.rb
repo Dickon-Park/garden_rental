bb59ab50ff62cf96913558d7081fa8d2c8c16d7d
raissa borba <raissavborba@gmail.com>
2020-07-22 17:34:38 +0200

    added booking path

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_07_21_174922) do
 
   # These are extensions that must be enabled in order to support this database
@@ -53,9 +52,6 @@ ActiveRecord::Schema.define(version: 2020_07_21_174922) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id"
-    t.float "latitude"
-    t.float "longitude"
-    t.string "address"
     t.decimal "price"
     t.integer "capacity"
     t.binary "image"
@@ -64,7 +60,9 @@ ActiveRecord::Schema.define(version: 2020_07_21_174922) do
     t.string "zipcode"
     t.string "street"
     t.string "number"
-
+    t.float "latitude"
+    t.float "longitude"
+    t.string "address"
     t.index ["user_id"], name: "index_gardens_on_user_id"
   end
 
