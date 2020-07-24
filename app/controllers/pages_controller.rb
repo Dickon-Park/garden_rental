0b501782f33cc0afce446207bd934e6870a86937
raissa borba <raissavborba@gmail.com>
2020-07-24 23:20:18 +0200

    search bar added on the navbar, index modified

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,16 +2,5 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   
   def home
-    if params[:query].present?
-      sql_query = " \
-        gardens.city @@ :query \
-        OR gardens.zipcode @@ :query \
-        OR gardens.name @@ :query \
-        OR gardens.description @@ :query \
-      "
-      @gardens = Garden.where(sql_query, query: "%#{params[:query]}%")
-    else
-      @gardens = Garden.all
-    end
   end
 end
